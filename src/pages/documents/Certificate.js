156127aad4c8739d82cad5451306415917949a07
zhangshaoju1987 <zhangshaoju.12@gmail.com>
2022-03-14 03:26:17 +0800

    3123

--- src/pages/documents/Certificate.js
+++ src/pages/documents/Certificate.js
@@ -1,7 +1,35 @@
-import React from "react";
-import { Text } from "react-native-paper";
-export default Certificate = () =>{
-    return (
-        <Text>证件类文档</Text>
-    );
-}
+import * as React from 'react';
+import { BottomNavigation, Text } from 'react-native-paper';
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+export default Certificate = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'music', title: 'Music', icon: 'target-account' },
+    { key: 'albums', title: 'Albums', icon: 'target-account' },
+    { key: 'recents', title: 'Recents', icon: 'target-account' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+  });
+
+  return (
+    <BottomNavigation
+      style={{display:"flex"}}
+      barStyle={{ height: 80,
+        backgroundColor: '#694fad',
+        justifyContent: 'center',}}
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
