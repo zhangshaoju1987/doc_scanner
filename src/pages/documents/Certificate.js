921eb05a7adf376d3e831d0f9943fa305ec70a8d
zhangshaoju1987 <zhangshaoju.12@gmail.com>
2022-03-15 21:31:51 +0800

    312313

--- src/pages/documents/Certificate.js
+++ src/pages/documents/Certificate.js
@@ -1,35 +1,10 @@
 import * as React from 'react';
-import { BottomNavigation, Text } from 'react-native-paper';
-
-const MusicRoute = () => <Text>测试页面1</Text>;
-
-const AlbumsRoute = () => <Text>测试页面2</Text>;
-
-const RecentsRoute = () => <Text>测试页面3</Text>;
+import { Text } from 'react-native-paper';
 
 export default Certificate = () => {
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: 'music', title: 'Music', icon: 'target-account' },
-    { key: 'albums', title: 'Albums', icon: 'target-account' },
-    { key: 'recents', title: 'Recents', icon: 'target-account' },
-  ]);
-
-  const renderScene = BottomNavigation.SceneMap({
-    music: MusicRoute,
-    albums: AlbumsRoute,
-    recents: RecentsRoute,
-  });
+ 
 
   return (
-    <BottomNavigation
-      style={{display:"flex",overflow:"hidden"}}
-      barStyle={{ height: 80,
-        backgroundColor: '#694fad',
-        justifyContent: 'center',}}
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-    />
+    <Text>展示拍摄的证件类文档</Text>
   );
 };
